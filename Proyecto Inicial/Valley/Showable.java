cb4cb04bb29a828ba0052844f699f2ee3c8f190f
Eduard Arias <eduarias08@gmail.com>
2019-08-27 19:07:39 -0500

    Showable interface created

---
+++ Proyecto Inicial/Valley/Showable.java
@@ -0,0 +1,21 @@
+
+/**
+ * Clase Showable que definidirá los comportamientos de todo objeto que
+ * se pueda mostrar en pantalla.
+ *
+ * @author Eduard Arias
+ * @version 1.0 (2019-08-27)
+ */
+public interface Showable{
+    
+    /**
+     * Muestra en pantalla el objeto.
+     */
+    public void makeVisible();
+    
+    /**
+     * Quita el objeto de pantalla.
+     */
+    public void makeInvisible();
+    
+}
